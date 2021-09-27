afdf3d4dc3586ea9ac958b87bc3bce31ad52e54a
Andrew Chapman <andrew.chapman@bc.edu>
2021-09-27 00:23:46 -0400

    5.9 Date formatting

--- ToDoList/ToDoDetailTableViewController.swift
+++ ToDoList/ToDoDetailTableViewController.swift
@@ -7,6 +7,14 @@
 
 import UIKit
 
+private let dateFormatter: DateFormatter = {
+    print("Just created the date formatter")
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateStyle = .short
+    dateFormatter.timeStyle = .short
+    return dateFormatter
+}()
+
 class ToDoDetailTableViewController: UITableViewController {
 
     @IBOutlet weak var noteView: UITextView!
@@ -27,7 +35,7 @@ class ToDoDetailTableViewController: UITableViewController {
         super.viewDidLoad()
         
         if toDoItem == nil {
-            toDoItem = ToDoItem(name: "", date: Date(), notes: "", reminderSet: false)
+            toDoItem = ToDoItem(name: "", date: Date().addingTimeInterval(24*60*60), notes: "", reminderSet: false)
         }
         
         updateUserInterface()
@@ -39,6 +47,7 @@ class ToDoDetailTableViewController: UITableViewController {
         noteView.text = toDoItem.notes
         reminderSwitch.isOn = toDoItem.reminderSet
         dateLabel.textColor = (reminderSwitch.isOn ? .black : .gray)
+        dateLabel.text = dateFormatter.string(from: toDoItem.date)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -57,6 +66,9 @@ class ToDoDetailTableViewController: UITableViewController {
         dateLabel.textColor = (reminderSwitch.isOn ? .black : .gray)
         tableView.beginUpdates()
         tableView.endUpdates()
+    }
+    @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+        dateLabel.text = dateFormatter.string(from: sender.date)
     }
 }
 
